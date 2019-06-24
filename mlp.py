b37e6e57398b429a5c9a1f8e0dbd61485f5bb1bf
Alla Nikhitha Reddy <2000104@wise>
2019-06-24 06:53:18 +0000

    combined the work until forward propagation

--- mlp.py
+++ mlp.py
@@ -15,14 +15,58 @@ class mlp:
             temp_bias = numpy.matrix(numpy.random.rand( 1 , number_of_neurons_in_each_layer [ i + 1 ]))
             self.weights.append(temp_weights)
             self.biases.append(temp_bias)
-        for i in self.weights:
-            print(i)
-        for i in self.biases:
-            print(i)
+    def sigmoid(self,value):
+        return 1/(1+numpy.exp(-value))
+
+
+    def tanh(self,value):
+        return (1-numpy.exp(-2*value))/(1+numpy.exp(-2*value))
+
+
+    def relu(self,value):
+        return max(0,value)
+    
+
+    def forward_propagation(self):
+        input_matrix = self.feature_matrix
+        for i in range(self.number_of_hidden_layers):
+             input_matrix = self.next_layer_input(input_matrix,self.weights[i],self.biases[i])
+        return  input_matrix
+
+
+    def next_layer_input(self,input_matrix,weight_matrix,bias_matrix):
+        output_before_activation= numpy.add( numpy.dot(input_matrix ,weight_matrix) , (bias_matrix))
+        next_input = self.apply_activation_function(output_before_activation)
+        return next_input
+
+
+    def apply_activation_function(self,output_before_activation):
+        if self.activation_function.lower() == "sigmoid":
+            return self.sigmoid(output_before_activation)
+        elif self.activation_function.lower() == "tanh":
+            return self.tanh(output_before_activation)
+        else:
+            return self.relu(output_before_activation)
 f1 = numpy.array([1 , 2, 3])
 f2 = numpy.array([1])
 f3 = [3 , 2 , 1]
-ob = mlp(f1 , f2 , f3)
+ob = mlp(f1 , f2 , f3 , "sigmoid")
+print(ob.forward_propagation())
+
  
+    
+
+
+
+
+        
+
+
+        
+
+
+
+
+        
 
 
