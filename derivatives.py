3d255d7ae59e7bd48fb210719304415d5a4f2144
Srinija Koppu <srinijasri12@gmail.com>
2019-06-24 12:01:23 +0000

    Derivatives of activation functions

--- derivatives.py
+++ derivatives.py
@@ -1,9 +1,15 @@
+import sys
+import numpy
+
 def sigmoid_derivative(self, value):
     return self.sigmoid(value) * (1 - self.sigmoid(value))
     
 def tanh_derivative(self, value):
-    return  1 - self.tanh(value) * self.tanh(value)
+    return  1 - (self.tanh(value) ** 2)
     
-def relu_derivative(self, value):     
-     return 1 * (self.relu_derivative(value) > 0)
+def relu_derivative(self, value):
+    if value >= 0 :
+        return 1
+    else :
+        return 0
     
