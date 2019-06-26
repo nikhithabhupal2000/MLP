8500145849ed5baef9aa6ab4ea0d06c5d240fa9d
Srinija Koppu <srinijasri12@gmail.com>
2019-06-26 03:34:45 +0000

    Update derivatives.py

--- derivatives.py
+++ derivatives.py
@@ -7,15 +7,8 @@ def sigmoid_derivative(self, value):
 def tanh_derivative(self, value):
     return  1 - (self.tanh(value) ** 2)
     
-<<<<<<< HEAD
-def relu_derivative(self, value):     
-     return max(sign(value), 0)
-    
-=======
 def relu_derivative(self, value):
     if value >= 0 :
         return 1
     else :
         return 0
-    
->>>>>>> 295c2ee8fb202d03605ca42b0d7736ad9e496035
