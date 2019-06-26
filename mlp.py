433d550faf32a6abe6343cb31d5d8d6ca41bbbdc
Alla Nikhitha Reddy <nikhithabhupal2000@gmail.com>
2019-06-26 08:19:04 +0000

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -1,13 +1,16 @@
 import numpy
-class lib_functions:
+
+
+
+class Activation_functions:
     def sigmoid(value):
-        return 1/(1+numpy.exp(-value))
+        return 1 / (1 + numpy.exp(-value))
 
     def tanh(value):
-        return (1-numpy.exp(-2*value))/(1+numpy.exp(-2*value))
+        return (1 - numpy.exp(-2 * value)) / (1 + numpy.exp(-2 * value))
 
     def relu(value):
-        return numpy.maximum(value,0)
+        return numpy.maximum(value, 0)
 
     def sigmoid_derivative(value):
         return numpy.asmatrix(numpy.asarray(value) * numpy.asarray(1 - value))
@@ -15,16 +18,18 @@ class lib_functions:
     def tanh_derivative(value):
         return  1 - numpy.asmatrix(numpy.asarray(value) * numpy.asarray(value))
     
-
     def relu_derivative(value):
         value[value <= 0] = 0
         value[value > 0] = 1
         return value
 
 
+
+
+
+
 class Mlp:
-    def __init__(self , feature_matrix , output_matrix , number_of_neurons_in_each_layer , testing_samples ,activation_function = "relu" , loss_function = "mean_square_error"):
-      
+    def __init__(self, feature_matrix, output_matrix, number_of_neurons_in_each_layer, testing_samples , weights, activation_function = "relu", loss_function = "mean_square_error"):  
         self.feature_matrix = feature_matrix
         self.output_matrix = output_matrix 
         self.number_of_neurons_in_each_layer = number_of_neurons_in_each_layer
@@ -37,56 +42,77 @@ class Mlp:
         self.loss_function = loss_function
         self.left_error = []
         self.solution = []
+        self.solution.append(self.testing_samples)
         self.learning_rate = 0.1
+
         for i in range(self.number_of_hidden_layers + 1):
-            temp_weights = numpy.matrix(numpy.random.rand(number_of_neurons_in_each_layer [ i ],number_of_neurons_in_each_layer [ i + 1]))
-            temp_bias = numpy.matrix(numpy.random.rand( 1 , number_of_neurons_in_each_layer [ i + 1 ]))
-            self.weights.append(temp_weights)
+            temp_bias = numpy.matrix(numpy.random.rand( 1 , number_of_neurons_in_each_layer [ i + 1]))
             self.biases.append(temp_bias)
+
+        if weights != []:
+            self.weights = weights
+        else:
+            for i in range(self.number_of_hidden_layers + 1):
+                temp_weights = numpy.matrix(numpy.random.rand(number_of_neurons_in_each_layer [ i ],number_of_neurons_in_each_layer[ i + 1]))
+                self.weights.append(temp_weights)
+        
+        
+
+
 
     def forward_propagation(self):
         self.prediction = []
         self.prediction.append(self.feature_matrix)
         for i in range(self.number_of_hidden_layers + 1):
-             self.prediction.append(self.next_layer_input(self.prediction[i],self.weights[i],self.biases[i]))
-    def predict(self):
-        self.solution . append(self.testing_samples)
-        for i in range(self.number_of_hidden_layers + 1):
-            self.solution.append(self.next_layer_input(self.solution[i] , self.weights[i] , self.biases[i]))
+             self.prediction.append(self.next_layer_prediction(self.prediction[i], self.weights[i], self.biases[i]))
 
+
+
+    def predict(self):
+        for i in range(self.number_of_hidden_layers + 1):
+            self.solution.append(self.next_layer_prediction(self.solution[i], self.weights[i], self.biases[i]))
         print(self.solution[-1])
 
 
-    def next_layer_input(self,input_matrix,weight_matrix,bias_matrix):
-        output_before_activation= numpy.add( numpy.matmul(input_matrix ,weight_matrix) , (bias_matrix))
-        next_input = self.apply_activation_function(output_before_activation)
-        return next_input
 
 
-    def derivative(self,predicted_output):
+    def next_layer_prediction(self, input_matrix, weight_matrix, bias_matrix):
+        prediction_before_activation = numpy.add(numpy.matmul(input_matrix, weight_matrix), (bias_matrix))
+        return  self.apply_activation_function(prediction_before_activation)
+        
+
+
+
+    def derivative(self, predicted_output):
         if self.activation_function.lower() == "sigmoid":
-            return lib_functions.sigmoid_derivative(predicted_output)
+            return Activation_functions.sigmoid_derivative(predicted_output)
         elif self.activation_function.lower() == "tanh":
-            return lib_functions.tanh_derivative(predicted_output)
+            return Activation_functions.tanh_derivative(predicted_output)
         else:
-            return lib_functions.relu_derivative(predicted_output)
+            return Activation_functions.relu_derivative(predicted_output)
 
-    def apply_activation_function(self,output_before_activation):
+
+
+
+    def apply_activation_function(self, prediction_before_activation):
         if self.activation_function.lower() == "sigmoid":
-            return lib_functions.sigmoid(output_before_activation)
+            return Activation_functions.sigmoid(prediction_before_activation)
         elif self.activation_function.lower() == "tanh":
-            return lib_functions.tanh(output_before_activation)
+            return Activation_functions.tanh(prediction_before_activation)
         else:
-            return lib_functions.relu(output_before_activation)
+            return Activation_functions.relu(prediction_before_activation)
 
 
-    def absolute_error(self,prediction , expected):
-        #print(numpy.subtract(prediction , expected))
-        return numpy.subtract(prediction , expected)
+
+
+    def absolute_error(self, prediction, expected):
+        return numpy.subtract(prediction, expected)
 	
 
-    def mean_square_error(self, prediction , expected):
-        return numpy.square(numpy.subtract(prediction , expected))/2
+    def mean_square_error(self, prediction, expected):
+        return numpy.square(numpy.subtract(prediction, expected)) / 2
+
+
 
     def apply_loss_function(self):
         if self.loss_function == "absolute_error":
@@ -95,54 +121,69 @@ class Mlp:
         else :
             
             return self.mean_square_error(self.output_matrix, self.prediction[-1])
+
+
+
+    def propagate_error(self, i):
+        return numpy.matmul(self.left_error[-1], numpy.transpose(self.weights[self.number_of_hidden_layers - i]))
         
-    def propagate_error(self , i):
-        return numpy.matmul(self.left_error[-1],numpy.transpose(self.weights[self.number_of_hidden_layers-i]))
-        
+
 
     def backward_propagation(self):
         self.left_error = []
-        self.left_error.append(numpy.asmatrix(numpy.asarray(self.derivative(self.prediction[-1]))*numpy.asarray( self.apply_loss_function())))
-        
-        
-        for i in range(self.number_of_hidden_layers ):        
-            self.left_error.append(numpy.asmatrix(numpy.asarray(self.derivative(self.prediction[self.number_of_hidden_layers - i ]) )* numpy.asarray( self.propagate_error(i))))
+        self.left_error.append(numpy.asmatrix(numpy.asarray(self.derivative(self.prediction[-1])) * numpy.asarray(self.apply_loss_function())))
+        for i in range(self.number_of_hidden_layers):        
+            self.left_error.append(numpy.asmatrix(numpy.asarray(self.derivative(self.prediction[self.number_of_hidden_layers - i])) * numpy.asarray(self.propagate_error(i))))
         
     
+
+
     def update_weights(self):
-        for i in range(self.number_of_hidden_layers  , -1 , -1):
-            self.weights[i] = self.weights[i] + numpy.matmul(numpy.transpose(self.prediction[i]) , self.left_error[self.number_of_hidden_layers - i])*self.learning_rate 
+        for i in range(self.number_of_hidden_layers, -1, -1):
+            self.weights[i] = self.weights[i] + numpy.matmul(numpy.transpose(self.prediction[i]), self.left_error[self.number_of_hidden_layers - i]) * self.learning_rate 
 
     def update_biases(self):
-        for i in range(self.number_of_hidden_layers , -1 , -1):
-            self.biases[i] = numpy.add(self.biases[i] , self.left_error[self.number_of_hidden_layers -i])*self.learning_rate 
+        for i in range(self.number_of_hidden_layers, -1, -1):
+            self.biases[i] = numpy.add(self.biases[i], self.left_error[self.number_of_hidden_layers - i]) * self.learning_rate 
 
     def update(self):
         self.update_weights()
         self.update_biases()
 
-X=numpy.matrix([[1,0,1,0],[1,0,1,1],[0,1,0,1]])
-y=numpy.matrix([[1],[0],[1]])
-f3 = [4,3,1]
-test = numpy.matrix([[0,0,1,1],[1,1,0,0],[1,1,1,1]])
-print(test)
-ob = Mlp(X,y,f3,test, "sigmoid" , "absolute_error")
-for i in range(100000):
+
+
+features = numpy.matrix([[1, 0, 1, 0], [1, 0, 1, 1], [0, 1, 0, 1]])
+actual_output = numpy.matrix([[1], [0], [1]])
+perceptrons_in_each_layer = [4, 3, 1]
+testing_sample = numpy.matrix([[0, 0, 1, 1], [1, 1, 0, 0], [1, 1, 1, 1]])
+ob = Mlp(features, actual_output, perceptrons_in_each_layer, testing_sample, [], "sigmoid" , "absolute_error")
+for i in range(1000):
     ob.forward_propagation()
     ob.backward_propagation()
     ob.update()
-print(ob.prediction[-1])
+
+print("Predicted outputs are ")
 ob.predict()
-#test = numpy.matrix([[1,0,0,0],[0,0,1,1],[1,1,1,1],[1,0,1,0]])
+
+#check for forward propagation
+
+features = numpy.matrix([[1, 2]])
+
+actual_output = numpy.matrix([[1]])
+
+perceptrons_in_each_layer = [2, 3, 3, 1]
+
+weights = [numpy.matrix([[0.4, 0, -1], [1, 2, 0]]), numpy.matrix([[0.6, 0.5, 0.7], [0.2, 0.3, 0.4], [0.5, 0.6, 0]]), numpy.matrix([[-1], [0], [1]])]
+
+ob = Mlp(features, actual_output, perceptrons_in_each_layer, [], weights, "sigmoid" , "absolute_error")
+ob.forward_propagation()
+print(ob.prediction[-1])
+     
 
 
 
 
         
 
-
-
-
-        
 
 
